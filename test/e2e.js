067aa1da7c744299c9fff02e19ef274e02d9e971
Pierre Colle <piercus@gmail.com>
2022-09-20 18:53:39 +0200

    fix: linting

--- test/e2e.js
+++ test/e2e.js
@@ -1,57 +1,51 @@
-const test = require('ava');
+const test = require('ava');// eslint-disable-line ava/no-ignored-test-files
 const {createDemuxerReadStream, createDecoderStream, createEncoderStream, createMuxerWriteStream, createFilterStream} = require('../lib/index.js');
-const beamcoder = require('beamcoder')
 
 test('e2e', t => {
-	const streamOpts = {highWaterMark: 1}
-	
-	const demuxerStream = createDemuxerReadStream('./test/Big_Buck_Bunny_first_23_seconds_1080p.ogv.480p.vp9.webm', streamOpts);
-	const streamIndexPromise = demuxerStream.demuxer.then(dm => dm.streams.find(x => x.codecpar.codec_type === 'video').index)
-	const filterStream = createFilterStream(a => streamIndexPromise.then(index => a.stream_index === index), streamOpts)
-	const decoderStream = createDecoderStream({demuxer: demuxerStream.demuxer, stream_index: 0}, streamOpts); // eslint-disable-line camelcase
-	
-	const params = demuxerStream.demuxer.then(dm => dm.streams.find(x => x.codecpar.codec_type === 'video').codecpar).then(a => {
-		return {
-			name: 'libx264',
-			width : a.width,
-	    height: a.height,
-			pix_fmt: a.format,
-			color_space: 'bt709',
-			time_base: [1, 90000]
-		}
-	});
-	const encoderStream = createEncoderStream(params, streamOpts);
-	
+	const streamOptions = {highWaterMark: 1};
+
+	const demuxerStream = createDemuxerReadStream('./test/Big_Buck_Bunny_first_23_seconds_1080p.ogv.480p.vp9.webm', streamOptions);
+	const streamIndexPromise = demuxerStream.demuxer.then(dm => dm.streams.find(x => x.codecpar.codec_type === 'video').index);
+	const filterStream = createFilterStream(a => streamIndexPromise.then(index => a.stream_index === index), streamOptions);
+	const decoderStream = createDecoderStream({demuxer: demuxerStream.demuxer, stream_index: 0}, streamOptions);
+
+	const parameters = demuxerStream.demuxer.then(dm => dm.streams.find(x => x.codecpar.codec_type === 'video').codecpar).then(a => ({
+		name: 'libx264',
+		width: a.width,
+		height: a.height,
+		pix_fmt: a.format,
+		color_space: 'bt709',
+		time_base: [1, 90_000],
+	}));
+	const encoderStream = createEncoderStream(parameters, streamOptions);
+
 	const muxerStream = createMuxerWriteStream({
 		filename: './tmp/file.mp4',
-		streams: encoderStream.encoder.then(enc => {
-			return [{ 
-				name: 'h264', 
-				time_base: params.time_base, 
-				encoderName: 'libx264'
-				// ,
-        /*codecpar: {
-          width: params.width, 
-					height: params.height, 
-					format: params.pix_fmt, 
-					color_space: params.color_space,
-          sample_aspect_ratio: [1, 1]
-        }*/
-      }];
-		}),
-	}, streamOpts);
-	
+		streams: encoderStream.encoder.then(parameters_ => [{
+			name: 'h264',
+			time_base: parameters_.time_base,
+			encoderName: 'libx264',
+			codecpar: {
+				width: parameters_.width,
+				height: parameters_.height,
+				format: parameters_.pix_fmt,
+				color_space: parameters_.color_space,
+				sample_aspect_ratio: [1, 1],
+			},
+		}]),
+	}, streamOptions);
+
 	const stream = demuxerStream.pipe(filterStream).pipe(decoderStream).pipe(encoderStream).pipe(muxerStream);
-	
+
 	let n = 0;
-	
+
 	return new Promise((resolve, reject) => {
 		stream.on('error', reject);
 		stream.on('close', resolve);
-		stream.on('data', data => {
+		stream.on('data', () => {
 			n++;
-		})
+		});
 	}).then(() => {
-		t.is(n, 554)
+		t.is(n, 554);
 	});
 });
--- test/test.js
+++ test/test.js
@@ -1,31 +1,29 @@
 const test = require('ava');
-const {createDemuxerReadStream, createDecoderStream, createEncoderStream, createMuxerWriteStream, createFilterStream, createFiltererStream} = require('../lib/index.js');
-const beamcoder = require('beamcoder')
+const {createDemuxerReadStream, createDecoderStream, createEncoderStream, createFilterStream, createFiltererStream} = require('../lib/index.js');
+
 test('demuxer', t => {
 	const demuxerStream = createDemuxerReadStream('./test/Big_Buck_Bunny_first_23_seconds_1080p.ogv.480p.vp9.webm');
 
-	const stream = demuxerStream;
 	let n = 0;
-
 
 	return new Promise((resolve, reject) => {
 		demuxerStream.on('error', reject);
 		demuxerStream.on('close', resolve);
-		demuxerStream.on('data', data => {
+		demuxerStream.on('data', () => {
 			n++;
-		})
+		});
 	}).then(() => {
-		t.is(n, 1719)
+		t.is(n, 1719);
 	});
 });
 
 test('demuxer-decoder', t => {
-	const streamOpts = {highWaterMark: 16}
-	
-	const demuxerStream = createDemuxerReadStream('./test/Big_Buck_Bunny_first_23_seconds_1080p.ogv.480p.vp9.webm', streamOpts);
-	const streamIndexPromise = demuxerStream.demuxer.then(dm => dm.streams.find(x => x.codecpar.codec_type === 'video').index)
-	const filterStream = createFilterStream(a => streamIndexPromise.then(index => a.stream_index === index), streamOpts)
-	const decoderStream = createDecoderStream({demuxer: demuxerStream.demuxer, stream_index: 0}, streamOpts); // eslint-disable-line camelcase
+	const streamOptions = {highWaterMark: 16};
+
+	const demuxerStream = createDemuxerReadStream('./test/Big_Buck_Bunny_first_23_seconds_1080p.ogv.480p.vp9.webm', streamOptions);
+	const streamIndexPromise = demuxerStream.demuxer.then(dm => dm.streams.find(x => x.codecpar.codec_type === 'video').index);
+	const filterStream = createFilterStream(a => streamIndexPromise.then(index => a.stream_index === index), streamOptions);
+	const decoderStream = createDecoderStream({demuxer: demuxerStream.demuxer, stream_index: 0}, streamOptions);
 
 	const stream = demuxerStream.pipe(filterStream).pipe(decoderStream);
 	let n = 0;
@@ -33,166 +31,146 @@ test('demuxer-decoder', t => {
 	return new Promise((resolve, reject) => {
 		stream.on('error', reject);
 		stream.on('close', resolve);
-		stream.on('data', data => {
+		stream.on('data', () => {
 			n++;
-		})
+		});
 	}).then(() => {
-		t.is(n, 555)
+		t.is(n, 555);
 	});
 });
 
 test('demuxer-decoder-encoder', t => {
-	const streamOpts = {highWaterMark: 1}
-	
-	const demuxerStream = createDemuxerReadStream('./test/Big_Buck_Bunny_first_23_seconds_1080p.ogv.480p.vp9.webm', streamOpts);
-	const streamIndexPromise = demuxerStream.demuxer.then(dm => dm.streams.find(x => x.codecpar.codec_type === 'video').index)
-	const filterStream = createFilterStream(a => streamIndexPromise.then(index => a.stream_index === index), streamOpts)
-	const decoderStream = createDecoderStream({demuxer: demuxerStream.demuxer, stream_index: 0}, streamOpts); // eslint-disable-line camelcase
-		
-	const params = demuxerStream.demuxer.then(dm => dm.streams.find(x => x.codecpar.codec_type === 'video').codecpar).then(a => {
-		return {
-			name: 'libx264',
-			width : a.width,
-	    height: a.height,
-			pix_fmt: a.format,
-			color_space: 'bt709',
-			time_base: [1, 90000]
-		}
-	});
-	
-	const encoderStream = createEncoderStream(params, streamOpts);
+	const streamOptions = {highWaterMark: 1};
+
+	const demuxerStream = createDemuxerReadStream('./test/Big_Buck_Bunny_first_23_seconds_1080p.ogv.480p.vp9.webm', streamOptions);
+	const streamIndexPromise = demuxerStream.demuxer.then(dm => dm.streams.find(x => x.codecpar.codec_type === 'video').index);
+	const filterStream = createFilterStream(a => streamIndexPromise.then(index => a.stream_index === index), streamOptions);
+	const decoderStream = createDecoderStream({demuxer: demuxerStream.demuxer, stream_index: 0}, streamOptions);
+
+	const parameters = demuxerStream.demuxer.then(dm => dm.streams.find(x => x.codecpar.codec_type === 'video').codecpar).then(a => ({
+		name: 'libx264',
+		width: a.width,
+		height: a.height,
+		pix_fmt: a.format,
+		color_space: 'bt709',
+		time_base: [1, 90_000],
+	}));
+
+	const encoderStream = createEncoderStream(parameters, streamOptions);
 
 	const stream = demuxerStream.pipe(filterStream).pipe(decoderStream).pipe(encoderStream);
-	
+
 	let n = 0;
 
 	return new Promise((resolve, reject) => {
 		stream.on('error', reject);
 		stream.on('close', resolve);
-		stream.on('data', data => {
+		stream.on('data', () => {
 			n++;
-		})
+		});
 	}).then(() => {
-		t.is(n, 505)
+		t.is(n, 505);
 	});
 });
 
 test('demuxer-decoder-filterer', t => {
-	const streamOpts = {highWaterMark: 1}
-	
-	const demuxerStream = createDemuxerReadStream('./test/Big_Buck_Bunny_first_23_seconds_1080p.ogv.480p.vp9.webm', streamOpts);
-	const streamIndexPromise = demuxerStream.demuxer.then(dm => dm.streams.find(x => x.codecpar.codec_type === 'video').index)
-	const filterStream = createFilterStream(a => streamIndexPromise.then(index => a.stream_index === index), streamOpts)
-	const decoderStream = createDecoderStream({demuxer: demuxerStream.demuxer, stream_index: 0}, streamOpts); // eslint-disable-line camelcase
-	
-	const codecparPromise = demuxerStream.demuxer.then(dm => dm.streams.find(x => x.codecpar.codec_type === 'video').codecpar)
-	
-	const filtererStream = createFiltererStream({
-	  filterType: 'video',
-	  inputParams: codecparPromise.then(a => ([
-	    {
-	      width: a.width,
-	      height: a.height,
-	      pixelFormat: a.format,
-	      timeBase:  [1, 90000],
-	      pixelAspect: a.sample_aspect_ratio,
-	    }
-	  ])),
-	  outputParams: codecparPromise.then(a => ([
-	    {
-	      pixelFormat: a.format
-	    }
-	  ])),
-	  filterSpec: 'scale=1280:720'
-	}, streamOpts);
+	const streamOptions = {highWaterMark: 1};
 
-	
-	const params = codecparPromise.then(a => {
-		return {
-			name: 'libx264',
-			width : a.width,
-	    height: a.height,
-			pix_fmt: a.format,
-			color_space: 'bt709',
-			time_base: [1, 90000]
-		}
-	});
-	
-	const encoderStream = createEncoderStream(params, streamOpts);
+	const demuxerStream = createDemuxerReadStream('./test/Big_Buck_Bunny_first_23_seconds_1080p.ogv.480p.vp9.webm', streamOptions);
+	const streamIndexPromise = demuxerStream.demuxer.then(dm => dm.streams.find(x => x.codecpar.codec_type === 'video').index);
+	const filterStream = createFilterStream(a => streamIndexPromise.then(index => a.stream_index === index), streamOptions);
+	const decoderStream = createDecoderStream({demuxer: demuxerStream.demuxer, stream_index: 0}, streamOptions);
+
+	const codecparPromise = demuxerStream.demuxer.then(dm => dm.streams.find(x => x.codecpar.codec_type === 'video').codecpar);
+
+	const filtererStream = createFiltererStream({
+		filterType: 'video',
+		inputParams: codecparPromise.then(a => ([
+			{
+				width: a.width,
+				height: a.height,
+				pixelFormat: a.format,
+				timeBase: [1, 90_000],
+				pixelAspect: a.sample_aspect_ratio,
+			},
+		])),
+		outputParams: codecparPromise.then(a => ([
+			{
+				pixelFormat: a.format,
+			},
+		])),
+		filterSpec: 'scale=1280:720',
+	}, streamOptions);
 
 	const stream = demuxerStream.pipe(filterStream).pipe(decoderStream).pipe(filtererStream);
-	
+
 	let n = 0;
 
 	return new Promise((resolve, reject) => {
 		stream.on('error', reject);
 		stream.on('close', resolve);
 		stream.on('data', data => {
-			t.is(data.length, 1)
-			t.is(data[0].height, 720)
-			t.is(data[0].width, 1280)
+			t.is(data.length, 1);
+			t.is(data[0].height, 720);
+			t.is(data[0].width, 1280);
 			n++;
-		})
+		});
 	}).then(() => {
-		t.is(n, 555)
+		t.is(n, 555);
 	});
 });
 
-
 test('demuxer-decoder-filterer-encoder', t => {
-	const streamOpts = {highWaterMark: 1}
-	
-	const demuxerStream = createDemuxerReadStream('./test/Big_Buck_Bunny_first_23_seconds_1080p.ogv.480p.vp9.webm', streamOpts);
-	const streamIndexPromise = demuxerStream.demuxer.then(dm => dm.streams.find(x => x.codecpar.codec_type === 'video').index)
-	const filterStream = createFilterStream(a => streamIndexPromise.then(index => a.stream_index === index), streamOpts)
-	const decoderStream = createDecoderStream({demuxer: demuxerStream.demuxer, stream_index: 0}, streamOpts); // eslint-disable-line camelcase
-	
-	const codecparPromise = demuxerStream.demuxer.then(dm => dm.streams.find(x => x.codecpar.codec_type === 'video').codecpar)
-	
-	const filtererStream = createFiltererStream({
-	  filterType: 'video',
-	  inputParams: codecparPromise.then(a => ([
-	    {
-	      width: a.width,
-	      height: a.height,
-	      pixelFormat: a.format,
-	      timeBase:  [1, 90000],
-	      pixelAspect: a.sample_aspect_ratio,
-	    }
-	  ])),
-	  outputParams: codecparPromise.then(a => ([
-	    {
-	      pixelFormat: a.format
-	    }
-	  ])),
-	  filterSpec: 'scale=1280:720'
-	}, streamOpts);
+	const streamOptions = {highWaterMark: 1};
 
-	
-	const params = codecparPromise.then(a => {
-		return {
-			name: 'libx264',
-			width : 1280,
-	    height: 720,
-			pix_fmt: a.format,
-			color_space: 'bt709',
-			time_base: [1, 90000]
-		}
-	});
-	
-	const encoderStream = createEncoderStream(params, streamOpts);
+	const demuxerStream = createDemuxerReadStream('./test/Big_Buck_Bunny_first_23_seconds_1080p.ogv.480p.vp9.webm', streamOptions);
+	const streamIndexPromise = demuxerStream.demuxer.then(dm => dm.streams.find(x => x.codecpar.codec_type === 'video').index);
+	const filterStream = createFilterStream(a => streamIndexPromise.then(index => a.stream_index === index), streamOptions);
+	const decoderStream = createDecoderStream({demuxer: demuxerStream.demuxer, stream_index: 0}, streamOptions);
+
+	const codecparPromise = demuxerStream.demuxer.then(dm => dm.streams.find(x => x.codecpar.codec_type === 'video').codecpar);
+
+	const filtererStream = createFiltererStream({
+		filterType: 'video',
+		inputParams: codecparPromise.then(a => ([
+			{
+				width: a.width,
+				height: a.height,
+				pixelFormat: a.format,
+				timeBase: [1, 90_000],
+				pixelAspect: a.sample_aspect_ratio,
+			},
+		])),
+		outputParams: codecparPromise.then(a => ([
+			{
+				pixelFormat: a.format,
+			},
+		])),
+		filterSpec: 'scale=1280:720',
+	}, streamOptions);
+
+	const parameters = codecparPromise.then(a => ({
+		name: 'libx264',
+		width: 1280,
+		height: 720,
+		pix_fmt: a.format,
+		color_space: 'bt709',
+		time_base: [1, 90_000],
+	}));
+
+	const encoderStream = createEncoderStream(parameters, streamOptions);
 
 	const stream = demuxerStream.pipe(filterStream).pipe(decoderStream).pipe(filtererStream).pipe(encoderStream);
-	
+
 	let n = 0;
 
 	return new Promise((resolve, reject) => {
 		stream.on('error', reject);
 		stream.on('close', resolve);
-		stream.on('data', data => {
+		stream.on('data', () => {
 			n++;
-		})
+		});
 	}).then(() => {
-		t.is(n, 505)
+		t.is(n, 505);
 	});
 });
